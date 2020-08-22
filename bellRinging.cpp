e771a3096407e30736dfcb3e83e416b5fae25de6
Jh142857 <69996166+Jh142857@users.noreply.github.com>
2020-08-22 10:11:01 +0800

    Add files via upload

--- bellRinging.cpp
+++ bellRinging.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <time.h>
 #include "vector"
-#include "std_msgs/Float32.h"
+#include "std_msgs/Float32MultiArray.h"
 #include "std_msgs/Float64MultiArray.h"
 #include "controller_manager_msgs/SwitchController.h"
 #include "controller_manager_msgs/ListControllers.h"
 #include "ikfast.h"
 #include "probot_anno_manipulator_ikfast_moveit_plugin.cpp"
+
+#define PI 3.14159
 using namespace std;
 void planOnce(double theta[],double w[],double v[]){
 double a1 = theta[0], a2 = theta[1], a3 = theta[2], a4 = theta[3], a5 = theta[4], a6 = theta[5];
@@ -29,10 +31,9 @@ w[5] = (v6*(412500*cos(a5) - 412500*pow(cos(a2),2)*cos(a5) + 1746507*pow(cos(a3)
                                                                             
 }
  
- 
-
-void terminalVelocity(double t,double v[]){
-    double dx = 0.0511, dy = -0.24, dz = -0.374, t_once = 4.9;
+void velocitySolve(double T, double v[], double dx, double dy, double dz){
+    // double dx = 0.0511, dy = -0.24, dz = -0.374, t_once = 4;
+    double t_once = 3;
     double dt= t_once / 7;
     double vx_max = dx / 4 / dt;
     double vx1 = dx / 16 /dt;
@@ -50,47 +51,47 @@ void terminalVelocity(double t,double v[]){
     double Jz = 2 * vz1 / dt /dt;
 
     // cout << ax_max << " " << ax_max << " " <<az_max <<endl;
-    if (t < dt){
-        v[0] = 0.5 * Jx * t * t;
-        v[1] = 0.5 * Jy * t * t;
-        v[2] = 0.5 * Jz * t * t;
+    if (T < dt){
+        v[0] = 0.5 * Jx * T * T;
+        v[1] = 0.5 * Jy * T * T;
+        v[2] = 0.5 * Jz * T * T;
     }
-    else if (t < 2 * dt){
-        v[0] = 0.5 * Jx * dt *dt + ax_max * (t - dt);
-        v[1] = 0.5 * Jy * dt *dt + ay_max * (t - dt);
-        v[2] = 0.5 * Jz * dt *dt + az_max * (t - dt);
+    else if (T < 2 * dt){
+        v[0] = 0.5 * Jx * dt *dt + ax_max * (T - dt);
+        v[1] = 0.5 * Jy * dt *dt + ay_max * (T - dt);
+        v[2] = 0.5 * Jz * dt *dt + az_max * (T - dt);
     }
-    else if (t < 3 *dt)
+    else if (T < 3 *dt)
     {
-        v[0] = vx_max - 0.5 * Jx * (3*dt - t) * (3*dt -t);
-        v[1] = vy_max - 0.5 * Jy * (3*dt - t)* (3*dt -t);
-        v[2] = vz_max - 0.5 * Jz * (3*dt - t)* (3*dt -t);
+        v[0] = vx_max - 0.5 * Jx * (3*dt - T) * (3*dt -T);
+        v[1] = vy_max - 0.5 * Jy * (3*dt - T)* (3*dt -T);
+        v[2] = vz_max - 0.5 * Jz * (3*dt - T)* (3*dt -T);
     }
-    else if(t < 4*dt){
+    else if(T < 4*dt){
         v[0] = vx_max;
         v[1] = vy_max;
         v[2] = vz_max;
     }
 
-    else if (t < 5 *dt)
+    else if (T < 5 *dt)
     {
-       v[0] = vx_max - 0.5 * Jx *(t - 4*dt) *(t - 4*dt);
-       v[1] = vy_max - 0.5 * Jy *(t - 4*dt) *(t - 4*dt);
-       v[2] = vz_max - 0.5 * Jz *(t - 4*dt) *(t - 4*dt);
+       v[0] = vx_max - 0.5 * Jx *(T - 4*dt) *(T - 4*dt);
+       v[1] = vy_max - 0.5 * Jy *(T - 4*dt) *(T - 4*dt);
+       v[2] = vz_max - 0.5 * Jz *(T - 4*dt) *(T - 4*dt);
     }
 
-    else if (t < 6 *dt)
+    else if (T < 6 *dt)
     {
-        v[0] = vx_max - 0.5 * Jx *dt * dt -ax_max*(t - 5 *dt);
-        v[1] = vy_max - 0.5 * Jy *dt * dt -ay_max*(t - 5 *dt);
-        v[2] = vz_max - 0.5 * Jz *dt * dt -az_max*(t - 5 *dt);
+        v[0] = vx_max - 0.5 * Jx *dt * dt -ax_max*(T - 5 *dt);
+        v[1] = vy_max - 0.5 * Jy *dt * dt -ay_max*(T - 5 *dt);
+        v[2] = vz_max - 0.5 * Jz *dt * dt -az_max*(T - 5 *dt);
     }
 
-    else if (t < t_once)
+    else if (T < t_once)
     {
-        v[0]= 0.5 * Jx * (t_once - t) *(t_once- t);
-        v[1] = 0.5 * Jy * (t_once - t) *(t_once - t);
-        v[2] = 0.5 * Jz * (t_once - t) *(t_once - t);
+        v[0]= 0.5 * Jx * (t_once - T) *(t_once- T);
+        v[1] = 0.5 * Jy * (t_once - T) *(t_once - T);
+        v[2] = 0.5 * Jz * (t_once - T) *(t_once - T);
     }
     else
     {
@@ -98,30 +99,45 @@ void terminalVelocity(double t,double v[]){
         v[1] = 0;
         v[2] = 0;
     }
+} 
 
-    
-    //4s
-    /*
-        if(t < 2){
-		v[0] = 0.0511 * t / 8;
-        v[1] = -0.03 * t;
-		v[2] =-0.374 * t /8;
+void terminalVelocity(double t,double v[]){
+     if(t < 1){
+		v[0] = 0.0511 * t / 2;
+        v[1] = -0.12 * t;
+		v[2] =-0.374 * t / 2;
 	}
-	else if(t < 4){
-		v[0] = 0.0511 / 4;
-        v[1] = -0.06;
-        v[2] = -0.374 / 4;
+	else if(t < 2){
+		v[0] = 0.0511 / 2;
+        v[1] = -0.12;
+        v[2] = -0.374 / 2;
     }
-	else if(t < 6){
-		v[0] = 0.0511 * (6 - t) / 8;
-        v[1] = -0.03 * (6 - t );
-        v[2] = -0.374 * (6 - t) / 8;
+	else if(t < 3){
+		v[0] = 0.0511 * (3 - t) / 2;/* code */
+        v[1] = -0.12 * (3 - t );
+        v[2] = -0.374 * (3 - t) / 2;
 	}
+    // else if (t < 5)
+    // {
+    //     // cout << "1" <<endl;
+    //     //  velocitySolve(t - 4,v,-0.02, 0.39, 0);
+    //     v[0] = 0.075* (t - 3) * (t -4) * (t - 5) * (t + 28 /15);
+    //     v[1] =  -0.2925 * (t -3) * (t -5);
+    //     v[2] = 0.4 *(t - 3) *(t - 4) * (t - 5);
+    // }
+    // else if (t < 7)
+    // {
+    //     // velocitySolve(t - 7, v, 0.02, -0.39, 0);
+    //     v[0] = -0.075 * (7 - t) * (6 - t) * (5 - t) * (10 - t + 28/15);
+    //     v[1] = 0.2925 * (t - 5) * (t - 7);
+    //     v[2] = 0.4 *(t - 7) *(t - 6) * (t - 5); 
+    // }
 	else{
 		v[0] = 0;
+        v[1] = 0;
 		v[2] = 0;
 	}
-    */
+
 }
 
 int main(int argc, char **argv) {
@@ -134,16 +150,18 @@ int main(int argc, char **argv) {
     ros::AsyncSpinner spinner(1);
     ROS_INFO_STREAM("start");
 
-    double RUNTIME = 4.9;
+    double RUNTIME = 3;
     double w[6], v[6] = {0, 0, 0, 0, 0, 0};
     static double theta[6] = {0,0,0,0,0,0};
 
     static double t = 0;
     double dt = 0.01;
-
-    ros::Publisher vel_pub = node_handle.advertise<std_msgs::Float64MultiArray>("/probot_anno/arm_vel_controller/command", 1000);
-    ros::Rate rate(50);
-    std_msgs::Float64MultiArray init_vel;
+    int count = 0;
+    ros::Publisher vel_pub = node_handle.advertise<std_msgs::Float32MultiArray>("/speed_chatter", 1000);
+    
+    // ros::Publisher vel_pub = node_handle.advertise<std_msgs::Float64MultiArray>("/probot_anno/arm_vel_controller/command", 1000);
+    ros::Rate rate(100);
+    std_msgs::Float32MultiArray init_vel;
     init_vel.data.push_back(0);
     init_vel.data.push_back(0);
     init_vel.data.push_back(0);
@@ -157,18 +175,22 @@ int main(int argc, char **argv) {
         cout << t << ":DO ONCE"<<  endl;
         /* code */
         terminalVelocity(t, v);
-
+      // cout<<"v"<<v[1];
         planOnce(theta, w, v);
+
+        if (t >= 7) {
+            t -= 4;
+        } 
         for (int i = 0; i < 6; i++)
         {
             theta[i] += w[i] * dt;
         }
-        init_vel.data.at(0) = w[0];
-        init_vel.data.at(1) = w[1];
-        init_vel.data.at(2) = w[2];
-        init_vel.data.at(3) = w[3];
-        init_vel.data.at(4) = w[4];
-        init_vel.data.at(5) = w[5];
+        init_vel.data.at(0) = w[0] * 30 *180 / PI;
+        init_vel.data.at(1) = w[1] *205 *180 /3/PI;
+        init_vel.data.at(2) = w[2] *50*180/PI;
+        init_vel.data.at(3) = w[3]*125 *180 /2/PI;
+        init_vel.data.at(4) = w[4]*125*180/2/PI;
+        init_vel.data.at(5) = w[5] *200*180/9/PI;
         for (int i=0; i < 6; i++){
             cout << "\nw:" <<  w[i] << " " << endl;
         }
@@ -176,6 +198,12 @@ int main(int argc, char **argv) {
 
         rate.sleep();
         t += dt;
+        count ++;
+        
+        if (count == 10000) {
+            break;
+        }
+        
     }
     ROS_INFO_STREAM("published");
 }
